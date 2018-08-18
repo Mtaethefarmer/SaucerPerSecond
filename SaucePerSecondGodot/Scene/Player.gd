46432b80ece908ff741a618b5ee95a23a52f66ab
Mtaethefarmer <mtae000@gmail.com>
2018-08-17 23:08:33 -0700

    Player movement added to the left and right

---
+++ SaucePerSecondGodot/Scene/Player.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+export (int) var speed = 5
+
+var velocity = Vector2()
+
+func _ready():
+	pass
+
+func _process(delta):
+	get_input()
+	apply_impulse(Vector2(), velocity)
+
+func get_input():
+    velocity = Vector2()
+    if Input.is_action_pressed('ui_right'):
+        velocity.x += 1
+    if Input.is_action_pressed('ui_left'):
+        velocity.x -= 1
+    velocity = velocity.normalized() * speed
