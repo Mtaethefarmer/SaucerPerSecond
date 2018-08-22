03904f0fe692cc0e6254bf7a499cfb9338f543c9
Mtaethefarmer <mtae000@gmail.com>
2018-08-22 16:12:50 -0700

    The game now goes back to the Main Menu when the song ends.
    Signed-off-by: Mtaethefarmer <mtae000@gmail.com>

--- SaucePerSecondGodot/Scripts/World.gd
+++ SaucePerSecondGodot/Scripts/World.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var MainMenu = load("res://Scenes/MainMenu.tscn")
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -9,3 +11,6 @@ func _process(delta):
 	$Walls.position.y = $Player.position.y
 	#$Interface.rect_position.y = $Player.position.y + $Player/Camera2D.offset.y
 	$Spawner.position.y = $Player.position.y + $Player/Camera2D.offset.y
+
+func _on_Music_finished():
+	get_tree().change_scene_to(MainMenu)
